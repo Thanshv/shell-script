b7410e50d9e04f60cfb69890d8f31c1a6184ba18
Thanshv <sravanij.1163@gmail.com>
2024-06-19 16:06:06 +0530

    hello script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How is your work?"
+echo "$PERSON2: not nad... but want to upgrade"
